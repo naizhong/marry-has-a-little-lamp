3b65585e7914aa0d94ee72641d61cf366c3b6f63
naizhong <xunaizhong@gmail.com>
2021-02-17 18:08:53 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+music.play_melody("E D C D E E E D", 120)
+music.play_melody("D D E G G E D C", 120)
+music.play_melody("D E E E E D D E", 120)
+music.play_melody("D C", 120)
---
+++ main.ts
@@ -0,0 +1,4 @@
+music.playMelody("E D C D E E E D", 120)
+music.playMelody("D D E G G E D C", 120)
+music.playMelody("D E E E E D D E", 120)
+music.playMelody("D C", 120)
